bdd3098100d4d22b05326fdf7333126358fc8fdc
AlejandroMtz07 <alejandro23juanito@gmail.com>
2024-11-17 21:30:44 -0700

    Cambios sobre el método luhn

--- src/luhn/Luhn.java
+++ src/luhn/Luhn.java
@@ -5,10 +5,10 @@ package luhn;
 public class Luhn {
     //Algoritmo de luhn para tarjetas con 16 dígitos
     public static void main(String[] args) {
-        int valores[] = {4,1,3,7,8,9,4,7,1,1,7,5,5,9,0,4};
-        System.out.println(luhn(valores));
+        System.out.println(luhn(4,1,3,7,8
+                ,9,4,7,1,1,7,5,5,9,0,4));
     }
-    public static String luhn(int[] valores) {
+    public static String luhn(int ... valores) {
         int total = 0;
         for (int i = 0; i < valores.length; i++) {
             if (i % 2 == 0) {
